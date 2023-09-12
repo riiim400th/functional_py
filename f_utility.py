fcc618035b9485602466684548ad6740c5f8295b
RiiiM <riiimparm+tech@gmail.com>
2023-09-13 05:51:49 +0900

    first commit

---
+++ f_utility.py
@@ -0,0 +1,35 @@
+def assoc(_d, key, value):
+    from copy import deepcopy
+    d = deepcopy(_d)
+    d[key] = value
+    return d
+
+def call(fn: function, key): 
+    """example:
+    id_slide = call(lambda x: x+10000, 'id')
+    capitalize_name = call(lambda x: x.title(), 'username')
+    username_modify_to_test = call(lambda x: '__test__.' + x, 'username')
+    """
+    def apply_fn(record):
+        return assoc(record, key, fn(record.get(key)))
+    return apply_fn
+
+def pipe_f(input_data: list,list_f: list): 
+    from functools import reduce
+    """example:
+    pipe_f(datadict,  [id_slide,
+                        capitalize_name, 
+                        username_modify_to_test])
+    """
+    return reduce(lambda f_data, f: map(f,f_data), list_f, input_data)
+
+'''
+id_slide = call(lambda x: x+10000, 'id')
+capitalize_name = call(lambda x: x.title(), 'username')
+username_modify_to_test = call(lambda x: '__test__.' + x, 'username')
+
+def modify_data(datadict):
+    return pipe_f(datadict,  [id_slide,
+                                capitalize_name, 
+                                username_modify_to_test])
+'''
